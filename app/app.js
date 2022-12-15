50d1189e2a687844abca56051fbf4ebc36b4dbc1
Mor Mordechai Ben <mor6686@gmail.com>
2022-12-15 10:52:19 +0200

    ☺

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 import { userRouter } from './routers/user.routes.js';
+
 // import { myKeys } from './config/keys.js';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
+
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
 
 app.use('/api', userRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 export { app };
